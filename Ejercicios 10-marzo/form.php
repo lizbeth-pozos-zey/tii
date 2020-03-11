fc458445d36c332c39dd177b6852a52443b1fc91
lizbeth-pozos-zey <56325021+lizbeth-pozos-zey@users.noreply.github.com>
2020-03-11 12:55:40 -0600

    Add files via upload
    11marzo2020

---
+++ Ejercicios 10-marzo/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>NOMBRE Y APELLIDO</title>
+</head>
+<body>
+<form name="formulario" method="POST" action="form2.php">
+
+<br>Nombre: <input type="text" name="nombre" value=""></br>
+<br>Apellido: <input type="text" name="apellido" value=""></br>
+<br><input type="submit" /></br>
+</form>
+</body>
+</html>
---
+++ Ejercicios 10-marzo/form2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DATOS INGRESADOS</title>
+</head>
+<body>
+<?php
+$nombre = $_POST['nombre'];
+echo $nombre;
+$apellido = $_POST['apellido'];
+echo $apellido;
+?>
+
+</body>
+</html>
+
