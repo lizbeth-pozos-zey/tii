fcd03794f2f93f655b613d47a96bd28b66f762eb
lizbeth-pozos-zey <56325021+lizbeth-pozos-zey@users.noreply.github.com>
2020-03-17 01:51:01 -0600

    practica 25
    practica25

--- PRACTICA-LPY-25/registrar.php
+++ PRACTICA-LPY-25/registrar.php
@@ -2,7 +2,7 @@
 include ("con_db.php");
 
 if (isset($_POST['register'])) {
-	if (strlen($_POST['name'] >= 1 && strlen($_POST['email'] >= 1))) {
+	if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
         $name =  trim ($_POST['name']);
         $email = trim ($_POST['email']);
         $fechareg = date("d/m/y");
